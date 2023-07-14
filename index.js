bd1269007a0d7722e7d8c7d5f61f50af051f80fc
Katie Sayers <kate@student.dev-island.org>
2023-07-13 14:38:35 -1000

    operations should all now meet specs of project

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ const decimal = document.querySelector('.decimal');
 const nums = document.querySelectorAll('.nums');
 const operators = document.querySelectorAll('.operator');
 
+let firstNum = '';
+let secondNum = '';
+let operator = '';
+let outcome = '';
+let maxLength = 8;
+
+const allClear = (e) => {
+    display.innerText = '0';
+    firstNum = '';
+    secondNum = '';
+    operator = '';
+    outcome = '';
+    console.log('Cleared')
+};
+
 const add = (x, y) => {
     return +x + +y;
 }
@@ -20,20 +35,39 @@ const multiply = (x, y) => {
     return x * y;
 }
 const divide = (x, y) => {
-    return x / y;
+    // if (y === 0) {
+    //     return "No can do";
+    // } else {
+        return x / y;
+    // }
 }
 
-const allClear = (e) => {
-    display.innerHTML = 0
-    console.log('clicked')
-};
+const operations = () => {
+    if(operator === '+') {
+        outcome = add(firstNum, secondNum)
+         } else if (operator === '-') {
+        outcome = subtract(firstNum, secondNum)
+         } else if (operator === 'x') {
+        outcome = multiply(firstNum, secondNum)
+        } else if (operator === '\u00F7') {
+        outcome = divide(firstNum, secondNum)
+            }
+        if (outcome.toString().length > 10) {
+            display.innerText = outcome.toFixed(3);
+        } else {
+            display.innerText = outcome;
+        }
+        console.log('OUTCOME', outcome);
+        secondNum = '';
+        operator = '';
+    }
 
 const displayNum = (e) => {
 if (!operator) {
     firstNum += e.target.innerText;
     display.innerText = firstNum;
     console.log('FIRST NUM', firstNum);
-} else if (operator) {
+} else if (operator && firstNum) {
     secondNum += e.target.innerText;
     display.innerText = secondNum;
     console.log('SECOND NUM', secondNum);
@@ -44,46 +78,21 @@ const displayOperator = (e) => {
     if (!operator && firstNum) {
         operator += e.target.innerText;
         console.log('OPERATOR', operator);
-    } 
+    } else if (operator) {
+        operator = ''
+        operator += e.target.innerText;
+        console.log('OPERATOR', operator);
+    }
 }
-
-let firstNum = '';
-let secondNum = '';
-let operator = '';
-let outcome = 0;
 
 const operate = (e) => {
     if (firstNum && secondNum && operator) {
            if (e.target.classList.contains('equals')) {
-            if(operator === '+') {
-            outcome = add(firstNum, secondNum)
-             } else if (operator === '-') {
-            outcome = subtract(firstNum, secondNum)
-             } else if (operator === 'x') {
-            outcome = multiply(firstNum, secondNum)
-                } else if (operator === '\u00F7') {
-            outcome = divide(firstNum, secondNum)
-             }
-            display.innerText = outcome
-            console.log('OUTCOME', outcome);
+           operations();
             firstNum = '';
-            secondNum = '';
-            operator = '';
         } else if (e.target.classList.contains('operator')) {
-            if(operator === '+') {
-                outcome = add(firstNum, secondNum)
-                 } else if (operator === '-') {
-                outcome = subtract(firstNum, secondNum)
-                 } else if (operator === 'x') {
-                outcome = multiply(firstNum, secondNum)
-                    } else if (operator === '\u00F7') {
-                outcome = divide(firstNum, secondNum)
-                 }
-                display.innerText = outcome
-                console.log('OUTCOME', outcome);
-                firstNum = outcome;
-                secondNum = '';
-                operator = '';
+            operations();
+             firstNum = outcome;
         }
     }
 }
